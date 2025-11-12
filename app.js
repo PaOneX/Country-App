e47b2433b462b5dbefdabbb5fe5b094b88517ba9
PaOneX <opcmicrosoft@gmail.com>
2025-11-12 15:02:47 +0530

    Gain New Look

--- app.js
+++ app.js
@@ -1,29 +1,43 @@
-console.log("hi")
+console.log("hi");
 
-function search(){
-    console.log("hi")
+function search() {
+    console.log("searching...");
 
-    let countryName = document.getElementById("searchCountry").value
+    let cityName = document.getElementById("searchCity").value;
+    let countryName = document.getElementById("searchCountry").value;
 
     fetch(`https://restcountries.com/v3.1/name/${countryName}`)
-    .then(res=>res.json())
-    .then(data =>{
-        document.getElementById("name").innerText = data[0].name.common
-        document.getElementById("flag").innerHTML = `<img src="${data[0].flags.png}" alt="flag"  width="350px">`
-        document.getElementById("officialName").innerText = data[0].name.official
-        document.getElementById("nativeName").innerText = Object.values(data[0].name.nativeName)[0].official    
-        document.getElementById("capital").innerText = data[0].capital[0]
-        document.getElementById("region").innerText = data[0].region
-        document.getElementById("area").innerText =  data[0].area
-        document.getElementById("population").innerText =  data[0].population
-        document.getElementById("continent").innerText = data[0].continents[0]
-        document.getElementById("currency").innerText = Object.keys(data[0].currencies)[0]
-        document.getElementById("language").innerText = Object.values(data[0].languages).join(", ")
-        document.getElementById("timeZone").innerText = data[0].timezones[0]
-        document.getElementById("location").innerText = data[0].maps.openStreetMaps
+        .then((res) => res.json())
+        .then((data) => {
+            const c = data[0] || {};
+            document.getElementById("name").innerText = (c.name && c.name.common) || "-";
+                    if (c.flags && c.flags.png) {
+                        document.getElementById("flag").innerHTML = `<img crossorigin="anonymous" src="${c.flags.png}" alt="flag" style="max-width:100%;height:auto;">`;
+                    } else {
+                        document.getElementById("flag").innerHTML = "";
+                    }
 
-    }
-
-    )
-
+                    // Coat of arms / symbol: set only if available, otherwise clear
+                    if (c.coatOfArms && c.coatOfArms.png) {
+                        document.getElementById("symbol").innerHTML = `<img crossorigin="anonymous" src="${c.coatOfArms.png}" alt="symbol" style="max-width:80px;height:auto;">`;
+                    } else {
+                        document.getElementById("symbol").innerHTML = "";
+                    }
+            document.getElementById("officialName").innerText = (c.name && c.name.official) || "-";
+            document.getElementById("nativeName").innerText = (c.name && c.name.nativeName && Object.values(c.name.nativeName)[0] && Object.values(c.name.nativeName)[0].official) || "-";
+            document.getElementById("capital").innerText = (c.capital && c.capital[0]) || "-";
+            document.getElementById("region").innerText = c.region || "-";
+            document.getElementById("area").innerText = c.area || "-";
+            document.getElementById("population").innerText = c.population ? c.population.toLocaleString() : "-";
+            document.getElementById("continent").innerText = (c.continents && c.continents[0]) || "-";
+            document.getElementById("currency").innerText = c.currencies ? Object.keys(c.currencies)[0] : "-";
+            document.getElementById("language").innerText = c.languages ? Object.values(c.languages).join(", ") : "-";
+            document.getElementById("timeZone").innerText = (c.timezones && c.timezones[0]) || "-";
+            document.getElementById("location").innerText = (c.maps && c.maps.openStreetMaps) || "-";
+        })
+        .catch((err) => {
+            console.error(err);
+            alert("Country not found or an error occurred");
+        });
 }
+    
